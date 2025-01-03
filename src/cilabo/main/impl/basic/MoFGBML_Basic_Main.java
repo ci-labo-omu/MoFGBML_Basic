56dffa9d88d6c4f1d6f84fc28487abaa047588d2
T <t@gmail.com>
2025-01-03 16:10:28 +0900

    Update

--- src/cilabo/main/impl/basic/MoFGBML_Basic_Main.java
+++ src/cilabo/main/impl/basic/MoFGBML_Basic_Main.java
@@ -265,6 +265,7 @@ public class MoFGBML_Basic_Main {
 		String outputRootDir = Consts.EXPERIMENT_ID_DIR;
 		new PittsburghSolutionListOutputX(nonDominatedSolutionsARC)
         .setVarFileOutputContext(new DefaultFileOutputContext(outputRootDir + sep + String.format("VARARC-%d.csv", Consts.TERMINATE_EVALUATION), ","))
+        .setFunFileOutputContext(new DefaultFileOutputContext(outputRootDir + sep + String.format("FUNARC-%d.csv", Consts.TERMINATE_EVALUATION), ","))
         .print();
 
         //バグ含むのでコメントアウト（修正するならJmetal仕様のメソッドを書き換える）
